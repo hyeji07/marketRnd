76e4c58e9c226b6d9596d3be1a67cd2a1ce8818e
hyeji07 <zindex_ann@naver.com>
2023-07-18 18:22:13 +0900

    feat: 캘린더 기본 컴포넌트 구현

--- src/components/calendar/CalendarCommon.tsx
+++ src/components/calendar/CalendarCommon.tsx
@@ -1,3 +1,23 @@
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import styles from './calendarCommon.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
 export default function CalendarCommon() {
-  return <div></div>;
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+
+  return (
+    <div>
+      {/* <p className={styles['date-picker']}></p> */}
+      <p className={cx('date-picker')}></p>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        //커스텀
+      />
+    </div>
+  );
 }
