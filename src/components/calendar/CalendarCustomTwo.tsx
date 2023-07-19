0449e6042faa65909476161b7379b1445b18161d
hyeji07 <zindex_ann@naver.com>
2023-07-19 18:54:32 +0900

    feat: 월,년도 선택 옵션 추가

--- src/components/calendar/CalendarCustomTwo.tsx
+++ src/components/calendar/CalendarCustomTwo.tsx
@@ -6,6 +6,7 @@ import ko from 'date-fns/locale/ko';
 
 import styles from './calendarCustomTwo.module.scss';
 import classNames from 'classnames/bind';
+import dayjs from 'dayjs';
 
 const cx = classNames.bind(styles);
 
@@ -14,8 +15,11 @@ registerLocale('ko', ko); //날짜 한국어로 표시
 export default function CalendarCustomTwo() {
   const [startDate, setStartDate] = useState<Date | null>(new Date());
 
+  const date = dayjs(startDate).format('YYYY-MM-DD');
+  // console.log(date);
+
   return (
-    <div>
+    <div className={cx('custom-wrap')}>
       <p>커스텀2</p>
       {/* <p className={styles['date-picker']}></p> */}
       <p className={cx('date-picker')}></p>
@@ -26,8 +30,15 @@ export default function CalendarCustomTwo() {
         locale='ko' //한국어로 설정
         // dateFormat='yyyy.MM.dd(eee)' //eeee는 '요일'까지 표시됨
         dateFormat='yyyy.MM.dd' //eeee는 '요일'까지
-        className='form-control'
-        dayClassName={(d) => cx('custom-day')}
+        className='form-control' //input클래스
+        dayClassName={(d) => cx('custom-day')} //달력일자클래스/무조건 함수로 넘겨야함
+        /* peekNextMonth :다음달 1주까지 달력표시*/
+
+        //월,년도 선택 옵션
+        showMonthDropdown
+        showYearDropdown
+        dropdownMode='select'
+        yearItemNumber={9}
       />
     </div>
   );
