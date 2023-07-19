20e7c723813058ed46b8e7657a538b7c93d08b5a
hyeji07 <zindex_ann@naver.com>
2023-07-19 18:55:32 +0900

    feat: 캘린더 컴포넌트 추가

--- src/pages/PaymentDetails.tsx
+++ src/pages/PaymentDetails.tsx
@@ -1,17 +1,26 @@
 import CalendarCommon from '@components/calendar/CalendarCommon';
+import CalendarCustomFour from '@components/calendar/CalendarCustomFour';
+import CalendarCustomThree from '@components/calendar/CalendarCustomThree';
 import CalendarCustomTwo from '@components/calendar/CalendarCustomTwo';
-import { DatetimePicker } from '@components/calendar/DateTimePicker';
+// import { DatetimePicker } from '@components/calendar/DateTimePicker_X';
 
 export default function PaymentDetails() {
   return (
     <div>
       <CalendarCommon />
 
-      <p>진행중</p>
       <CalendarCustomTwo />
 
-      <p>보류</p>
-      <DatetimePicker type='date' />
+      <p>커스텀3: 스타일 적용</p>
+      <CalendarCustomThree />
+
+      <p>커스텀4:</p>
+      <div>
+        <CalendarCustomFour />
+      </div>
+
+      {/* <p>보류</p> */}
+      {/* <DatetimePicker type='date' /> */}
     </div>
   );
 }
