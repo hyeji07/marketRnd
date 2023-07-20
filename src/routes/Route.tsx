893a7386636ef3715ac3ce9dc994f46c54c20c5c
hyeji07 <zindex_ann@naver.com>
2023-07-20 12:44:59 +0900

    feat: 캘린더 페이지 추가

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -4,6 +4,7 @@ import Home from '@pages/Home';
 import LayoutDefault from '@layout/LayoutDefault';
 import WrittenReview from '@pages/WrittenReview';
 import PaymentDetails from '@pages/PaymentDetails';
+import CalendarEx from '@pages/CalendarEx';
 
 export default function AppRoute(): JSX.Element {
   return (
@@ -12,6 +13,8 @@ export default function AppRoute(): JSX.Element {
         <Route path='/' element={<Home />} />
         <Route path='/written_review' element={<WrittenReview />} />
         <Route path='/payment_details' element={<PaymentDetails />} />
+        {/*react-datepicker 라이브러리 적용예제*/}
+        <Route path='/calendar' element={<CalendarEx />} />
       </Route>
     </Routes>
   );
