8289f69278be4f270c896c0c207ed29ba0d42a53
hyeji07 <zindex_ann@naver.com>
2023-07-20 19:00:51 +0900

    feat: Header 컴포넌트 추가

--- src/layout/LayoutDefault.tsx
+++ src/layout/LayoutDefault.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import './layoutDefault.scss';
+import Header from '@components/layouts/Header';
 
 export default function LayoutDefault() {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
