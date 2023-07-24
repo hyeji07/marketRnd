29da89299af586268a07d7f3e622d50a86cb792e
hyeji07 <zindex_ann@naver.com>
2023-07-24 18:47:43 +0900

    feat: beauty페이지 생성

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -5,12 +5,14 @@ import LayoutDefault from '@layout/LayoutDefault';
 import WrittenReview from '@pages/WrittenReview';
 import PaymentDetails from '@pages/PaymentDetails';
 import CalendarEx from '@pages/CalendarEx';
+import Beauty from '@pages/Beauty';
 
 export default function AppRoute(): JSX.Element {
   return (
     <Routes>
       <Route element={<LayoutDefault />}>
         <Route path='/' element={<Home />} />
+        <Route path='/beauty' element={<Beauty />} />
         <Route path='/written_review' element={<WrittenReview />} />
         <Route path='/payment_details' element={<PaymentDetails />} />
         {/*react-datepicker 라이브러리 적용예제*/}
