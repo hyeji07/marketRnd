dc4e562dccd14a6cec39acdc1bd4f857ca8fd500
hyeji07 <zindex_ann@naver.com>
2023-08-02 18:10:11 +0900

    feat: ProductItem 컴포넌트 적용

--- src/components/carousel/MultipleCarousel.tsx
+++ src/components/carousel/MultipleCarousel.tsx
@@ -4,11 +4,9 @@ import 'swiper/css';
 import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
 import { CarouselInterface } from '@interfaces/components/CarouselInterface';
-import SmsOutlinedIcon from '@mui/icons-material/SmsOutlined';
 import './multipleCarousel.scss';
-import calculatePercent from './../../helpers/calculatePercent';
+import ProductItem from '@components/product/ProductItem';
 
 export default function MultipleCarousel({
   items,
@@ -32,64 +30,7 @@ export default function MultipleCarousel({
         {items.map((item, idx) => {
           return (
             <SwiperSlide key={idx}>
-              <div className='product-wrap'>
-                <div
-                  className='product-img'
-                  style={{
-                    backgroundImage: `url(${item.url})`,
-                  }}
-                >
-                  {/* 주말특가 같은 마커표시 */}
-                  {item.marker && (
-                    <span className='product-marker'>{item.marker}</span>
-                  )}
-                  <button className='product-cart'></button>
-                </div>
-                <div className='product-info'>
-                  <h4 className='product-name'>{item.title}</h4>
-
-                  {item.percent && item.discountPrice ? (
-                    <div>
-                      <p className='product-discount-price'>
-                        {/* <span className='product-percent'>{item.percent}%</span> */}
-                        <span className='product-percent'>
-                          {calculatePercent(item.price, item.discountPrice)}%
-                        </span>
-                        {item.discountPrice
-                          .toString()
-                          .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                        원
-                      </p>
-                      <p className='product-price-dimmed'>
-                        {item.price
-                          .toString()
-                          .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                        원
-                      </p>
-                    </div>
-                  ) : (
-                    <p className='product-price'>
-                      {item.price
-                        .toString()
-                        .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                      원
-                    </p>
-                  )}
-                  {item.review && item.review !== 0 ? (
-                    <div className='product-review'>
-                      <SmsOutlinedIcon className='product-review-icon' />
-                      후기
-                      <span className='product-review-number'>
-                        {item.review > 9999
-                          ? '9,999+'
-                          : item.review
-                              .toString()
-                              .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                      </span>
-                    </div>
-                  ) : null}
-                </div>
-              </div>
+              <ProductItem item={item} />
             </SwiperSlide>
           );
         })}
